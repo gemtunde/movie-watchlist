5320c53400338c1284e334c4f4f71f703ac67fa4
gemtunde <gemtunde@gmail.com>
2022-09-26 19:26:48 +0100

    add movie

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+
+import Add from './components/Add';
+import Header from './components/Header';
+import Watched from './components/Watched';
+import Watchlist from './components/Watchlist';
 import './App.css';
+import './lib/font-awesome/css/all.min.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <Router>
+       <Header />
+
+       <Switch>
+          <Route exact path='/'>
+              <Watchlist />
+          </Route>
+          <Route path='/watched'>
+              <Watched />
+          </Route>
+          <Route path='/add'>
+              <Add />
+          </Route>
+       </Switch>
+   </Router>
   );
 }
 
